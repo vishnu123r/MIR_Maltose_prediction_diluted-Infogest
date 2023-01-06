a4edc0d25401c3fbfc3726a54c0fade0866cc8fb
Ramanah Visnupriyan <s4712582@uq.edu.au>
2023-01-06 15:57:29 +1000

    Added code to plot Q residuals vs T^2 plots

--- mir_plsr.py
+++ mir_plsr.py
@@ -6,6 +6,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 
+from sklearn.cross_decomposition import PLSRegression
+from scipy.stats import f
+
+
 drop_columns = ['Technical_rep']
 
 #Read CSV and Drop columns
@@ -42,35 +46,83 @@ y = df_SN['maltose_concentration'].values
 
 X = savgol_filter(X, 11, 2, 2)
 
-#Apply PLSR
-y_c, y_cv, score_c, score_cv, rmse_c, rmse_cv, x_load = conduct_pls(4, X, y)
 
-#plot y range values 
-rangey = max(y) - min(y)
-rangex_c = max(y_c) - min(y_c)
-rangex_cv = max(y_cv) - min(y_cv)
+#### To get Q^2 vs T^2 plot
+ncomp = 4
+# Define PLS object
+pls = PLSRegression(n_components=ncomp)
+# Fit data
+pls.fit(X, y)
+ 
+# Get X scores
+T = pls.x_scores_
+# Get X loadings
+P = pls.x_loadings_
+ 
+# Calculate error array
+Err = X - np.dot(T,P.T)
+ 
+# Calculate Q-residuals (sum over the rows of the error array)
+Q = np.sum(Err**2, axis=1)
+ 
+# Calculate Hotelling's T-squared (note that data are normalised by default)
+Tsq = np.sum((pls.x_scores_/np.std(pls.x_scores_, axis=0))**2, axis=1)
 
-# Fit a line to the CV vs response
-z_c = np.polyfit(y, y_c, 1)
-z_cv = np.polyfit(y, y_cv, 1)
+# set the confidence level
+conf = 0.95
+ 
 
+# Calculate confidence level for T-squared from the ppf of the F distribution
+Tsq_conf =  f.ppf(q=conf, dfn=ncomp, \
+            dfd=X.shape[0])*ncomp*(X.shape[0]-1)/(X.shape[0]-ncomp)
+ 
+# Estimate the confidence level for the Q-residuals
+i = np.max(Q)+1
+while 1-np.sum(Q>i)/np.sum(Q>0) > conf:
+    i -= 1
+Q_conf = i
+
+ax = plt.figure(figsize=(8,4.5))
 with plt.style.context(('ggplot')):
-    fig, ax = plt.subplots(figsize=(9, 5))
-    ax.scatter(y_c, y, c='red', edgecolors='k')
-    #Plot the best fit line (calibration)
-    ax.plot(np.polyval(z_c,y), y, c='blue', linewidth=1)
-    #Plot the best fit line (cross validation)
-    ax.plot(np.polyval(z_cv,y), y, c='yellow', linewidth=1)
-    #Plot the ideal 1:1 line
-    ax.plot(y, y, color='green', linewidth=1)
-    plt.title('$R^{2}$ (CV): '+str(round(score_cv,2)))
-    plt.xlabel('Predicted Maltose Concentration ()')
-    plt.ylabel('Measured Maltose Concentration ()')
+    plt.plot(Tsq, Q, 'o')
+ 
+    plt.plot([Tsq_conf,Tsq_conf],[plt.axis()[2],plt.axis()[3]],  '--')
+    plt.plot([plt.axis()[0],plt.axis()[1]],[Q_conf,Q_conf],  '--')
+    plt.xlabel("Hotelling's T-squared")
+    plt.ylabel('Q residuals')
+ 
+plt.show()
 
-    plt.show()
+# #Apply PLSR
+# y_c, y_cv, score_c, score_cv, rmse_c, rmse_cv, x_load = conduct_pls(4, X, y)
+
+# #plot y range values 
+# rangey = max(y) - min(y)
+# rangex_c = max(y_c) - min(y_c)
+# rangex_cv = max(y_cv) - min(y_cv)
+
+# # Fit a line to the CV vs response
+# z_c = np.polyfit(y, y_c, 1)
+# z_cv = np.polyfit(y, y_cv, 1)
+
+
+# with plt.style.context(('ggplot')):
+#     fig, ax = plt.subplots(figsize=(9, 5))
+#     ax.scatter(y_c, y, c='red', edgecolors='k')
+#     #Plot the best fit line (calibration)
+#     ax.plot(np.polyval(z_c,y), y, c='blue', linewidth=1)
+#     #Plot the best fit line (cross validation)
+#     ax.plot(np.polyval(z_cv,y), y, c='yellow', linewidth=1)
+#     #Plot the ideal 1:1 line
+#     ax.plot(y, y, color='green', linewidth=1)
+#     plt.title('$R^{2}$ (CV): '+str(round(score_cv,2)))
+#     plt.xlabel('Predicted Maltose Concentration ()')
+#     plt.ylabel('Measured Maltose Concentration ()')
+
+#     plt.show()
     
 
-print('R2 calib: %5.3f'  % score_c)
-print('R2 CV: %5.3f'  % score_cv)
-print('RMSE calib: %5.3f' % rmse_c)
-print('RMSE CV: %5.3f' % rmse_cv)
+# print('R2 calib: %5.3f'  % score_c)
+# print('R2 CV: %5.3f'  % score_cv)
+# print('RMSE calib: %5.3f' % rmse_c)
+# print('RMSE CV: %5.3f' % rmse_cv)
